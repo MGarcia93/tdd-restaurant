b474cfdcbc55daa81439233af8cf0759e89e3855
“MGarcia93” <maximiliano_garcia93@hotmail.com>
2022-10-04 20:52:09 -0300

    create migration and seeder basics

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['name' => 'ensalada'],
+            ['name' => 'pastas'],
+            ['name' => 'pizza']
+        ];
+        DB::table('categories')->insert($data);
+    }
+}
