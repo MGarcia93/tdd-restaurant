aa36759e2f1de00249bd592e4c417bae7d5563a7
“MGarcia93” <maximiliano_garcia93@hotmail.com>
2022-10-04 22:00:15 -0300

    create testing category in red

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpFaker();
+    }
+
+
+    public function test_canCreateACategory()
+    {
+        $data = [
+            "name" => $this->faker->sentence()
+        ];
+        $response = $this->post('/api/category', $data);
+        $response->assertStatus(201);
+    }
+    public function test_canGetAllCategories()
+    {
+        $categories = Category::all();
+        $response = $this->getJson('/api/category');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            "categories" => compact('categories')
+        ]);
+    }
+    public function test_canUpdateCategory()
+    {
+        $category = Category::factory()->create();
+        $category->name = "Update testing";
+        $response = $this->putJson("/api/category/{$category->id}", ["name" => $category->name]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('categories', [
+            "id" => $category->id,
+            "name" => $category->name
+        ]);
+    }
+    public function test_canDeleteCategory()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->get("/api/category/{$category->id}");
+        $response->assertStatus(200);
+        $response = $this->deleteJson("/api/category/{$category->id}");
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('categories', [
+            "id" => $category->id
+        ]);
+    }
+}
